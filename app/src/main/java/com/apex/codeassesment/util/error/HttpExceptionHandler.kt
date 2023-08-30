776da8782817f30c3e2b9d3dd6e9b7e1bf70e595
shahzadafridi <shahzadahmadafridi@gmail.com>
2023-08-30 17:23:12 +0500

    Exception util classes added to handle api exception

---
+++ app/src/main/java/com/apex/codeassesment/util/error/HttpExceptionHandler.kt
@@ -0,0 +1,33 @@
+package com.apex.codeassesment.util.error
+
+import com.apex.codeassesment.R
+import com.apex.codeassesment.util.resource.ResourceManager
+import retrofit2.HttpException
+import java.io.IOException
+
+class HttpExceptionHandler(
+    private val resourceManager: ResourceManager
+) {
+    suspend fun <T> wrap(block: suspend () -> T): T {
+        return try {
+            block()
+        } catch (e: Throwable) {
+            throw transformException(e)
+        }
+    }
+
+    fun transformException(exception: Throwable): BaseException {
+        return when (exception) {
+            is HttpException -> {
+                val response = exception.response()!!
+
+                val errorCode = response.code()
+                response.errorBody()?.close()
+
+                BaseException.httpError(errorCode, resourceManager.getString(R.string.undefined_error_message))
+            }
+            is IOException -> BaseException.networkError(resourceManager.getString(R.string.connection_error_message), exception)
+            else -> BaseException.unexpectedError(exception)
+        }
+    }
+}
